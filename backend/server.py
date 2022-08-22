f883acd5d7dbc311e327d548163a8f22d4f3b306
Igorithm <amit1994biswas@gmail.com>
2022-08-23 01:56:56 +0530

    check

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import util
-
 app = Flask(__name__)
 
 @app.route('/get_location_names', methods=['GET'])
@@ -9,7 +8,6 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
@@ -23,7 +21,6 @@ def predict_home_price():
         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
